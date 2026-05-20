dc97925369d6dfdc37005a38f816d55fec687280
Dragos Munteanu <97401353+dragosmx@users.noreply.github.com>
2026-05-20 11:01:11 +0300

    Initialize Firebase configuration in firebase-init.js

---
+++ firebase-init.js
@@ -0,0 +1,12 @@
+ const firebaseConfig = {
+            apiKey: "AIzaSyBHfyZzJ-GUfUUqEidMytaCSh-B2SYh1Ks",
+            authDomain: "killer-mentenanta.firebaseapp.com",
+            databaseURL: "https://killer-mentenanta-default-rtdb.europe-west1.firebasedatabase.app",
+            projectId: "killer-mentenanta",
+            storageBucket: "killer-mentenanta.firebasestorage.app",
+            messagingSenderId: "434460062600",
+            appId: "1:434460062600:web:38a2ff00266c3c1dff403d"
+        };
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}

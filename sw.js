9cf52f84a6de75ffd16b60eb6e06dda09bc10886
Dragos Munteanu <97401353+dragosmx@users.noreply.github.com>
2026-09-01 16:32:57 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'killer-mentenanta-v5';
+const CACHE_NAME = 'killer-mentenanta-v5.1';
 
 // ⚠️ Am adăugat librăriile externe. Fără ele, HTML-ul se încarcă degeaba offline.
 const urlsToCache = [
